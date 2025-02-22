d1b1c1683278cf559226d5220753dfd824f1f9cc
catcoder <catcoder.php@gmail.com>
2025-02-21 18:38:00 -0600

    Fri Feb 21 18:38:00 CST 2025

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
+echo "EJECUTANDO SCRIPT"
 rm -rf package
-pip install -r requirements.txt
 mkdir package
 pip install -r requirements.txt -t package/
 rm -rf lambda_function.zip
@@ -8,6 +8,5 @@ cp -r App package
 cd package
 zip -r ../lambda_function.zip .
 cd ..
-rm -rf package
 aws lambda update-function-code --function-name meliProductsAPI --zip-file fileb://lambda_function.zip >> /tmp/null
-rm -rf lambda_function.zip
+rm -rf package
